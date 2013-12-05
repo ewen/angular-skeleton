4d8b4cb8ecc7d471912e32071e8c6041b216a74c
Ewen Cumming <ewen.cumming@gmail.com>
2013-12-05 22:31:58 +1300

    Added some initial angular code.

---
+++ public/assets/scripts/bootstrap.js
@@ -0,0 +1,1 @@
+var angularSkeleton = angular.module('angularSkeleton', []);
---
+++ public/assets/scripts/controllers/MainCtrl.js
@@ -0,0 +1,3 @@
+angularSkeleton.controller('MainCtrl', function($scope) {
+	$scope.countdown = ['three', 'two', 'one', 'go'];
+});
